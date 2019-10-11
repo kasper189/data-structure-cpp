63d5e535711ed4dad866e6400872f2d84e654845
Davide Sanapo <davide.sanapo@amadeus.com>
2019-10-11 10:31:59 +0200

    [Heap] Added heap skeleton

---
+++ src/heap/Heap.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <stdlib.h>
+#include <vector>
+#include <cstddef>
+#include <cstdint>
+#include <stdexcept>
+
+namespace datastruct { namespace heap
+{
+  template<typename T>
+  class Heap {
+
+  public:
+    /** Constructor **/
+    Heap() {}
+
+    /** Destructor **/
+    ~Heap() {}
+  };
+
+}}
---
+++ tests/heap_test/HeapTest.cpp
@@ -0,0 +1,14 @@
+#include "gtest/gtest.h"
+
+#include <string>
+#include <stdlib.h>
+#include <stdexcept>
+
+#include "Heap.hpp"
+
+using datastruct::heap::Heap;
+
+TEST(HeapTest, InitTest)
+{
+	ASSERT_TRUE(true);
+}
